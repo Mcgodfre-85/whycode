3f0eca7fc3dbf6254070d9b4f0e4291b00f3c066
Athony A <astremsky@gmail.com>
2023-04-03 22:57:02 -0400

    Styles and about page

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express'
 import config from 'config'
-import  engine  from 'express-handlebars'
+import { engine } from 'express-handlebars'
 import { Configuration, OpenAIApi } from 'openai'
-import  favicon  from 'serve-favicon'
+import path from 'path'
+import favicon from 'serve-favicon'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express()
-app.use(favicon(path.join(__dirname,'/favicon.ico')))
+
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 app.use(express.urlencoded({ extended: true }))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 const configuration = new Configuration({
   apiKey: config.get('OPEN_AI_KEY')
@@ -21,19 +26,20 @@ app.get('/', (_, res) => {
   res.render('index')
 })
 
+app.get('/about', (_, res) => {
+  res.render('about')
+})
 
 app.post('/', async (req, res) => {
-  let loading = true
   const code = req.body.content
   const content = `
-    What this code does?
+    Explain what this code does? 
     ${code}
   `
-  
+
   const messages = [{ role: 'user', content }]
 
   try {
-    
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages,
@@ -42,12 +48,15 @@ app.post('/', async (req, res) => {
     res.render('index', {
       code,
       message: completion.data.choices[0].message,
-      loading = false
+      
     })
-
   } catch (e) {
-    console.error(e)
+    console.error(e.response)
+    res.render('index', {
+      code,
+      errorMessage: `Error: ${e.response.status} / ${e.response.statusText} / ${e.response.data.error.message}`,
+    })
   }
 })
 
-app.listen(4000)
+app.listen(4400)
